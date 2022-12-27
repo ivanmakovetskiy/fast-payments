b7a292489de65b61a6313ba8646fbec61bb81746
Ivan Makovetskiy <ivan@makovetskiy.xyz>
2022-12-27 04:47:21 +0300

    Comments

--- src/main/scala/fastpayments/FastPaymentsApp.scala
+++ src/main/scala/fastpayments/FastPaymentsApp.scala
@@ -10,13 +10,44 @@ import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.ExecutionContextExecutor
 
 object FastPaymentsApp extends App {
+  /**
+   * An implicit `ActorSystem` instance is required for running the HTTP server.
+   */
   implicit val system: ActorSystem = ActorSystem("FastPaymentsApp")
+
+  /**
+   * An implicit `ExecutionContextExecutor` instance is required for running futures.
+   */
   implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+  /**
+   * An implicit `Database` instance is required for interacting with the database.
+   */
   implicit val db: PostgresProfile.backend.Database = Database.forConfig("database.postgres")
 
+  /**
+   * Creates the database schema if it does not already exist.
+   */
   new InitDB().prepare()
+
+  /**
+   * Instantiates a new `AccountRepositoryDB` instance for interacting with the database.
+   */
   val repository = new AccountRepositoryDB
+
+  /**
+   * Instantiates a new `HelloRoute` instance and binds it to the `helloRoute` variable.
+   */
   private val helloRoute = new HelloRoute().route
+
+  /**
+   * Instantiates a new `AccountRoute` instance and binds it to the `accountRoute` variable.
+   */
   private val accountRoute = new AccountRoute(repository).route
+
+  /**
+   * Creates a new HTTP server at the specified host and port and binds it to the `helloRoute` and `accountRoute`
+   * variables.
+   */
   Http().newServerAt("0.0.0.0", port = 8081).bind(helloRoute ~ accountRoute)
 }
