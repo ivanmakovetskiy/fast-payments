a551e48b2e90b79deeb4b77873d375cf9c74cc88
Ivan Makovetskiy <ivan@makovetskiy.xyz>
2023-06-26 16:53:16 +0300

    Re-did Account class for managing instances

--- src/main/scala/fastpayments/model/Account.scala
+++ src/main/scala/fastpayments/model/Account.scala
@@ -1,13 +1,17 @@
 package fastpayments.model
+
 import java.util.UUID
 
 /**
  * Represents an account in a bank.
  *
- * @param id the unique identifier for this account
- * @param balance the current balance of this account
+ * @param id      The unique identifier for this account. If not provided, a random UUID will be generated.
+ * @param balance The current balance of this account.
  */
-case class Account(id: UUID = UUID.randomUUID(), balance: Int = 0)
+case class Account(
+                    id: UUID = UUID.randomUUID(),
+                    balance: Int = 0
+                  )
 
 /**
  * Represents a request to create a new account.
@@ -15,26 +19,14 @@ case class Account(id: UUID = UUID.randomUUID(), balance: Int = 0)
 case class CreateAccount()
 
 /**
- * Represents a request to add money to an account.
+ * Represents a request to update an existing account.
  *
- * @param id the unique identifier of the account to add money to
- * @param amount the amount of money to add to the account
+ * @param id       The unique identifier of the account.
+ * @param username An optional new username for the account.
+ * @param sum      An optional new sum for the account.
  */
-case class ReplenishAccount(id: UUID, amount: Int)
-
-/**
- * Represents a request to withdraw money from an account.
- *
- * @param id the unique identifier of the account to withdraw money from
- * @param amount the amount of money to withdraw from the account
- */
-case class WithdrawAccount(id: UUID, amount: Int)
-
-/**
- * Represents a request to transfer money from one account to another.
- *
- * @param idFrom the unique identifier of the account to transfer money from
- * @param idTo the unique identifier of the account to transfer money to
- * @param amount the amount of money to transfer
- */
-case class TransferMoney(idFrom: UUID, idTo: UUID, amount: Int)
+case class UpdateAccount(
+                          id: UUID,
+                          username: Option[String] = None,
+                          sum: Option[Float] = None
+                        )
